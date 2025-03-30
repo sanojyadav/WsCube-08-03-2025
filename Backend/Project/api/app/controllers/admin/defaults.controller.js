e82476e1949d497333d019a2d7de3855f365b9b1
Sanoj Kumar <sanojyadav09@gmail.com>
2025-03-30 17:19:42 +0530

    28-03-2025

--- Backend/Project/api/app/controllers/admin/defaults.controller.js
+++ Backend/Project/api/app/controllers/admin/defaults.controller.js
@@ -1,10 +1,13 @@
 const defaultSchema = require('../../models/default');
+const mongodb = require('mongodb');
 
 //  For Create Data
 exports.create = async(request,response) => {
     
     var dataSave = {
         name : request.body.name,
+        type : request.body.type,
+        email : request.body.email,
         order : request.body.order,
     }
     
@@ -18,6 +21,92 @@ exports.create = async(request,response) => {
     
         response.send(result);
     })
+    .catch((error) => {
+        console.log(error);
+
+        var error_messages = [];
+        var errorKey ={};
+
+        for(var data in error.errors){
+            console.log(error.errors[data].properties.message);
+
+            // error_messages.push(error.errors[data].properties.message);
+
+            errorKey[data] = error.errors[data].properties.message;
+            error_messages.push(errorKey);
+        }
+
+        const result = {
+            _status : false,
+            _message : 'Something went wrong !!',
+            error_messages : error_messages,
+            _data :  null
+        }
+    
+        response.send(result);
+    })
+}
+
+//  For View Data
+exports.view = async(request,response) => {
+    
+    const condition = {
+        deleted_at : null,
+    }
+
+    if(request.body.name != '' && request.body.name != undefined){
+        // var nameRegex = new RegExp("^" + request.body.name);
+        var nameRegex = new RegExp(request.body.name,"i");
+        condition.name = nameRegex
+    }
+    
+    // await defaultSchema.find(condition).select('name status order')
+    // .sort({
+    //     order : 'asc',
+    //     _id : 'desc'
+    // })
+
+
+    // await defaultSchema.findOne({ _id : '67e6c0df8db5c9f380fd1266' })
+
+    // await defaultSchema.findById('67e6c0df8db5c9f380fd1266')
+    // await defaultSchema.find({ order: { $gt: 9 } })
+
+    var limit = 4;
+    var skip = 0;
+    var page = 1;
+
+    if(request.body.page != '' && request.body.page != undefined){
+        page = request.body.page;
+    }
+
+    skip = (page - 1) * limit;
+
+    // await defaultSchema.find(condition).limit(limit).skip(skip)
+
+    // Model.find({role: { $exists: true } });
+
+    await defaultSchema.find({ name : { $type : 8 } })
+    .then((resp) => {
+        if(resp.length > 0){
+            const result = {
+                _status : true,
+                _message : 'Record inserted succussfully',
+                _data :  resp
+            }
+        
+            response.send(result);
+        } else {
+            const result = {
+                _status : false,
+                _message : 'No record found.',
+                _data :  []
+            }
+        
+            response.send(result);
+        }
+        
+    })
     .catch(() => {
         const result = {
             _status : false,
@@ -27,13 +116,6 @@ exports.create = async(request,response) => {
     
         response.send(result);
     })
-
-    
-}
-
-//  For View Data
-exports.view = (request,response) => {
-
 }
 
 //  For Details Data
@@ -52,6 +134,33 @@ exports.changeStatus = (request,response) => {
 }
 
 //  For Delete Data
-exports.destroy = (request,response) => {
+exports.destroy = async(request,response) => {
 
+    await defaultSchema.updateOne(
+        {
+            _id : request.body.id
+        },
+        {
+            $set : {
+                deleted_at : Date.now()
+            }
+        }
+    ).then((result) => {
+        const data = {
+            _status : true,
+            _message : 'Record updated succussfully',
+            _data :  result
+        }
+    
+        response.send(data);
+
+    }).catch((error) => {
+        const data = {
+            _status : false,
+            _message : 'Something went wrong !!',
+            _data :  ''
+        }
+    
+        response.send(data);
+    })
 }
--- Backend/Project/api/app/models/default.js
+++ Backend/Project/api/app/models/default.js
@@ -3,13 +3,36 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
     name : {
         type : String,
+        // minLength: 3,
+        // maxLength  : 10,
+        match: /^[a-zA-Z]{3,10}$/,
+        required : [true, 'Name is required'],
+        validate: {
+            validator: async function(v) {
+              const user = await this.constructor.findOne({ name: v });
+              return !user;
+            },
+            message: props => `The specified name is already in use.`
+        }
     },
+    email : {
+        type : String,
+        // unique: true,
+        validate: [validateEmail, 'Please fill a valid email address']
+    },
+    // type : {
+    //     type : String,
+    //     required : [true, 'Type is required'],
+    //     enum : ['admin','user']
+    // },
     status : {
         type : Boolean,
         default : true
     },
     order : {
         type : Number,
+        min : 0,
+        max : 1000,
         default : 0
     },
     created_at : {
@@ -25,6 +48,11 @@ const schema = new mongoose.Schema({
         default : ''
     }
 });
+
+function validateEmail(email) {
+    // Your custom validation logic here
+    return /\S+@\S+\.\S+/.test(email);
+}
 
 const model = mongoose.model('default',schema);
 
